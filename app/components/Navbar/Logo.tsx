1d91e56cef6983367e4aaa26caf5616340a061f3
Emmanuel Keifala <54929076+EmmanuelKeifala@users.noreply.github.com>
2023-06-09 12:00:59 +0000

    Update Logo.tsx

--- app/components/Navbar/Logo.tsx
+++ app/components/Navbar/Logo.tsx
@@ -10,7 +10,7 @@ const Logo = () => {
 		<Image
 			onClick={() => router.push("/")}
 			className="hidden md:block cursor-pointer"
-			src="/images/logo.png"
+			src=""
 			height="100"
 			width="100"
 			alt="Logo"
